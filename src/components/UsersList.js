802833ec3a056da3394bee65f92239e26df82bef
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-02-27 15:07:12 +0000

    show user.length in UsersList, added Promise pause
    if statements to account for all loading phases
    
    added new Promise to fetchUsers thunk to add a delay to loading to see each loading step - DEV ONLY!

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,13 +1,24 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
   const dispatch = useDispatch();
 
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
-  return <div>user list</div>;
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+  return <div>{data.length}</div>;
 }
 export default UsersList;
